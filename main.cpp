3c72e961f9c59cba10bbee1ce963ab04674bff14
Abouraya11 <youssefabouraya7@gmail.com>
2023-10-23 14:10:32 +0300

    [Development] Adds the minimum vector value functionality.

--- main.cpp
+++ main.cpp
@@ -12,12 +12,27 @@ int getSum(const vector<int>& numVector) {
     return sum;
 }
 
+int getMin(const vector<int>& numVector) {
+    if (numVector.empty()) {
+        return 0;
+    }
+    int min = numVector[0];
+    for (int num : numVector) {
+        if (num < min) {
+            min = num;
+        }
+    }
+    return min;
+}
+
 int main() {
     vector<int> numVector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int sum = getSum(numVector);
+    int min = getMin(numVector);
 
     cout << "Sum of elements in the vector: " << sum << std::endl;
+    cout << "Minimum element in the vector: " << min << endl;
 
     return 0;
 }
